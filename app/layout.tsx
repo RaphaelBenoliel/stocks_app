d3266a7b38ad61d9682c31d31ff54deeb03c499a
Raphael Benoliel <raphael2gb@gmail.com>
2025-10-20 11:50:23 +0300

    chore: update dependencies and add type definitions for nodemailer and react-select-country-list
    - Added several Radix UI components to dependencies.
    - Included better-auth, cmdk, inngest, mongodb, mongoose, next-themes, nodemailer, react-hook-form, react-select-country-list, and sonner to dependencies.
    - Created type definition file for nodemailer.
    - Created type definition file for react-select-country-list.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -13,8 +14,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Signalist Stocks App",
-  description: "Track real- time stock prices, get personalized alerts, and make informed investment decisions with Signalist Stocks App.",
+  title: "Signalist",
+  description: "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
 };
 
 export default function RootLayout({
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
---
+++ types/react-select-country-list.d.ts
@@ -0,0 +1,11 @@
+declare module 'react-select-country-list' {
+  interface Country {
+    label: string;
+    value: string;
+  }
+
+  export default function countryList(): {
+    getData: () => Country[];
+    getLabels: () => string[];
+  };
+}
